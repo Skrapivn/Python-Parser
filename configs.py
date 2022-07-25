3a80ad2107894f728a684f8076f8a811cb2fab68
Sergey Krapivnitskiy <ya.uplink@yandex.ru>
2022-07-25 22:13:35 +0300

    Python parser

---
+++ configs.py
@@ -0,0 +1,11 @@
+import argparse
+
+
+def configure_argument_parser(available_modes):
+    parser = argparse.ArgumentParser(description='Парсер документации Python')
+    parser.add_argument(
+        'mode',
+        choices=available_modes,
+        help='Режимы работы парсера'
+    )
+    return parser
